1fec313d78986e179c04e8b752863938aa7ea7d4
William Guay <2430063@collegebart.ca>
2025-04-29 13:32:38 -0400

    a

--- src/main.c
+++ src/main.c
@@ -5,9 +5,21 @@
 #include <assert.h>
 #include "utils.h"
 
+#include "stb_image.h"
+#include "stb_image_write.h"
+
 #define HEAP_SIZE 2048 * 2048 * 4
 static uint8_t* heap = NULL;
 static size_t heap_top = 0;
+
+const char* img_names[] = {
+	"31.bmp",
+	"64.bmp",
+	"128.bmp"
+};
+
+const size_t img_name_len = sizeof(img_names) / sizeof(img_names[0]);
+
 void* allocate(size_t size) {
 	size_t old_top = heap_top;
 	heap_top += size;
@@ -15,8 +27,34 @@ void* allocate(size_t size) {
 	return &heap[old_top];
 }
 
+int GetColor(int Channel) {
+	if (Channel == 0) return 0; // Si c'est noir
+	return 1; // Si c'est blanc
+}
+
 int main(int argc, char** argv) {
 	heap = (uint8_t*)malloc(HEAP_SIZE);
 	assert(heap != NULL);
 	
+	int width, height, channels;
+	unsigned char *img = stbi_load("31.bmp", &width, &height, &channels, 0);
+	if(img == NULL) {
+		printf("Error in loading the image\n");
+		exit(1);
+	}
+
+	printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
+	for (int i = 0; i < width * height * channels; i += channels) {
+		// Pour les couleurs : 0 = noir, 1 = blanc.
+		unsigned char r = img[i];
+		if (r == 255) {
+			img[i + 1] = 0;
+			img[i + 2] = 0;
+		}
+
+	}
+
+	char filepath[_MAX_PATH] = { 0 };
+	snprintf(filepath, _MAX_PATH, "solution_%s", img_names[0]);
+	stbi_write_bmp(filepath, width, height, channels, img);
 }
